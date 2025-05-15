5a7c15027a1f1e8b6d1b7c69027d11814d4de5fc
unknown <kaylahahla1306@gmail.com>
2025-05-15 15:13:37 +0700

    Updates

--- bundle.js
+++ bundle.js
@@ -511,7 +511,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		__webpack_require__.p = "/";
+/******/ 		__webpack_require__.p = "/storyapp/";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */
